4aac98d30358d9435e22affa6b45906877625f85
houtaib <bnjmbarka1953@gmail.com>
2024-07-16 12:16:19 +0100

    h

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+type Artist struct {
+	Id int `json:"id"`
+	Image string `json:"image"`
+	Name string `json:"name"`
+	Members string `json:"members"`
+	CreationDate int `json:"creationDate"`
+	FirstAlbum string `json:"firstAlbum"`
+	Locations string `json:"locations"`
+	ConcertDates string `json "concerDates"`
+	Relations string `json:"relations"`
+}
+
+
+
+func main() {
+
+}
