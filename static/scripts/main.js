33664afe5af90f74d747955e36ad3b0122cc050b
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-07-18 16:51:14 +0100

    add something

---
+++ static/scripts/main.js
@@ -0,0 +1,8 @@
+const bodyElement = document.body
+const headerElement = document.querySelector("header")
+const darkModeBtn = document.querySelector(".dark-mode-btn")
+
+darkModeBtn.addEventListener("click", () => {
+    console.log("Welcome")
+    bodyElement.classList.toggle("dark")
+})
