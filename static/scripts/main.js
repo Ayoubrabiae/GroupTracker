900d3d6c15f39f85377e5b0f94da296eb8d9d1ca
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-07-21 10:57:49 +0100

    add static

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,17 +2,37 @@ const bodyElement = document.body
 const headerElement = document.querySelector("header")
 const darkModeBtn = document.querySelector(".dark-mode-btn")
 
+const localStorageToggle = (key) => {
+    if (localStorage.getItem(key)) {
+        localStorage.setItem(key, "")
+    } else {
+        localStorage.setItem(key, "true")
+    }
+}
+
 // Dark mode functionality
+if (localStorage.getItem("dark")) {
+    bodyElement.classList.add("dark")
+}
+
 darkModeBtn.addEventListener("click", () => {
-    console.log("Welcome")
+    localStorageToggle("dark")
+    console.log(localStorage.getItem("dark"))
     bodyElement.classList.toggle("dark")
     darkModeBtn.children[0].classList.toggle("bxs-sun")
 })
-localStorage.setItem("darkMode", "true")
-console.log(localStorage.getItem("darkMode"))
 
 // Hero functionality
-const artistImages = ["queen.jpeg", "soja.jpeg", "pinkfloyd.jpeg", "scorpions.jpeg", "xxxtentacion.jpeg", "macmiller.jpeg", "joynerlucas.jpeg", "kencricklamar.jpeg", "acdc.jpeg", "pearljam.jpeg", "katyperry.jpeg", "rihanna.jpeg", "genesis.jpeg", "philcollins.jpeg", "ledzeppelin.jpeg", "jimihendrix.jpeg", "beegees.jpeg", "deeppurple.jpeg", "aerosmith.jpeg", "direstraits.jpeg", "30.jpeg", "imagineDragons.jpeg", "juiceWrld.jpeg", "logic.jpeg", "alecBeenjamin.jpeg", "bobbyMcFerrin.jpeg", "R3HAB.jpeg", "postmalone.jpeg", "travisScott.jpeg", "jCole.jpeg", "nickelback.jpeg", "mobbDeep.jpeg", "gunsNRoses.jpeg", "nwa.jpeg", "u2.jpeg", "arcticmonkeys.jpeg", "fallOutBoy.jpeg", "gorillaz.jpeg", "eagles.jpeg", "linkinpark.jpeg", "redhotchilipeppers.jpeg", "eminem.jpeg", "greenday.jpeg", "metallica.jpeg", "coldplay.jpeg", "maroon5.jpeg", "twentyonepilots.jpeg", "therollingstones.jpeg", "muse.jpeg", "foofighters.jpeg", "thechainsmokers.jpeg"]
+const artistImages = ["queen.jpeg", "soja.jpeg", "pinkfloyd.jpeg", "scorpions.jpeg",
+                    "xxxtentacion.jpeg", "macmiller.jpeg", "joynerlucas.jpeg", "kencricklamar.jpeg",
+                    "acdc.jpeg", "pearljam.jpeg", "katyperry.jpeg", "rihanna.jpeg", "genesis.jpeg",
+                    "philcollins.jpeg", "ledzeppelin.jpeg", "jimihendrix.jpeg", "beegees.jpeg", "deeppurple.jpeg",
+                    "aerosmith.jpeg", "direstraits.jpeg", "30.jpeg", "imagineDragons.jpeg", "juiceWrld.jpeg", "logic.jpeg",
+                    "alecBeenjamin.jpeg", "bobbyMcFerrin.jpeg", "R3HAB.jpeg", "postmalone.jpeg", "travisScott.jpeg", "jCole.jpeg",
+                    "nickelback.jpeg", "mobbDeep.jpeg", "gunsNRoses.jpeg", "nwa.jpeg", "u2.jpeg", "arcticmonkeys.jpeg", "fallOutBoy.jpeg",
+                    "gorillaz.jpeg", "eagles.jpeg", "linkinpark.jpeg", "redhotchilipeppers.jpeg", "eminem.jpeg", "greenday.jpeg", "metallica.jpeg",
+                    "coldplay.jpeg", "maroon5.jpeg", "twentyonepilots.jpeg", "therollingstones.jpeg", "muse.jpeg", "foofighters.jpeg", "thechainsmokers.jpeg"]
+
 const heroImageElements = document.querySelectorAll(".hero .images img")
 
 setInterval(() => {
