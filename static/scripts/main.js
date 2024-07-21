5a6066c0ad85cf9bc3c270998fad1be810a0461d
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-07-21 09:47:11 +0100

    add some animations

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -8,15 +8,16 @@ darkModeBtn.addEventListener("click", () => {
     bodyElement.classList.toggle("dark")
     darkModeBtn.children[0].classList.toggle("bxs-sun")
 })
+localStorage.setItem("darkMode", "true")
+console.log(localStorage.getItem("darkMode"))
 
 // Hero functionality
-const artistNames = ["queen.jpeg", "soja.jpeg", "pinkfloyd.jpeg", "scorpions.jpeg", "xxxtentacion.jpeg", "macmiller.jpeg", "joynerlucas.jpeg", "kencricklamar.jpeg", "acdc.jpeg", "pearljam.jpeg", "katyperry.jpeg", "rihanna.jpeg", "genesis.jpeg", "philcollins.jpeg", "ledzeppelin.jpeg", "jimihendrix.jpeg", "beegees.jpeg", "deeppurple.jpeg", "aerosmith.jpeg", "direstraits.jpeg", "30.jpeg", "imagineDragons.jpeg", "juiceWrld.jpeg", "logic.jpeg", "alecBeenjamin.jpeg", "bobbyMcFerrin.jpeg", "R3HAB.jpeg", "postmalone.jpeg", "travisScott.jpeg", "jCole.jpeg", "nickelback.jpeg", "mobbDeep.jpeg", "gunsNRoses.jpeg", "nwa.jpeg", "u2.jpeg", "arcticmonkeys.jpeg", "fallOutBoy.jpeg", "gorillaz.jpeg", "eagles.jpeg", "linkinpark.jpeg", "redhotchilipeppers.jpeg", "eminem.jpeg", "greenday.jpeg", "metallica.jpeg", "coldplay.jpeg", "maroon5.jpeg", "twentyonepilots.jpeg", "therollingstones.jpeg", "muse.jpeg", "foofighters.jpeg", "thechainsmokers.jpeg"]
-const heroImages = document.querySelectorAll(".hero .images img")
+const artistImages = ["queen.jpeg", "soja.jpeg", "pinkfloyd.jpeg", "scorpions.jpeg", "xxxtentacion.jpeg", "macmiller.jpeg", "joynerlucas.jpeg", "kencricklamar.jpeg", "acdc.jpeg", "pearljam.jpeg", "katyperry.jpeg", "rihanna.jpeg", "genesis.jpeg", "philcollins.jpeg", "ledzeppelin.jpeg", "jimihendrix.jpeg", "beegees.jpeg", "deeppurple.jpeg", "aerosmith.jpeg", "direstraits.jpeg", "30.jpeg", "imagineDragons.jpeg", "juiceWrld.jpeg", "logic.jpeg", "alecBeenjamin.jpeg", "bobbyMcFerrin.jpeg", "R3HAB.jpeg", "postmalone.jpeg", "travisScott.jpeg", "jCole.jpeg", "nickelback.jpeg", "mobbDeep.jpeg", "gunsNRoses.jpeg", "nwa.jpeg", "u2.jpeg", "arcticmonkeys.jpeg", "fallOutBoy.jpeg", "gorillaz.jpeg", "eagles.jpeg", "linkinpark.jpeg", "redhotchilipeppers.jpeg", "eminem.jpeg", "greenday.jpeg", "metallica.jpeg", "coldplay.jpeg", "maroon5.jpeg", "twentyonepilots.jpeg", "therollingstones.jpeg", "muse.jpeg", "foofighters.jpeg", "thechainsmokers.jpeg"]
+const heroImageElements = document.querySelectorAll(".hero .images img")
 
 setInterval(() => {
-    for (let i = 0; i < heroImages.length; i++) {
-        const randomNum = Math.floor(Math.random() * artistNames.length)
-        const imageSrc = `https://groupietrackers.herokuapp.com/api/images/${artistNames[randomNum]}`
-        heroImages[i].src = imageSrc
-    }
+    const randomElementNum = Math.floor(Math.random() * heroImageElements.length) 
+    const randomImageNum = Math.floor(Math.random() * artistImages.length)
+    const imageSrc = `https://groupietrackers.herokuapp.com/api/images/${artistImages[randomImageNum]}`
+    heroImageElements[randomElementNum].src = imageSrc
 }, 10000)
