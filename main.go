67f6907ab75c7f820c7b7ec07d840684ab13af31
Ayoub RABIE <105998148+Ayoubrabiae@users.noreply.github.com>
2024-08-13 10:54:59 +0100

    Update main.go

--- main.go
+++ main.go
@@ -26,11 +26,11 @@ func main() {
 		return
 	}
 
-	port := os.Getenv("PORT")
+	//port := os.Getenv("PORT")
 
-	if port == "" {
+	//if port == "" {
 		port = "8080"
-	}
+	//}
 
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/artists/", handlers.ProfileHandler)
